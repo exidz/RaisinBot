aa4df12788b72bb67dadf5ed757b11490d37d365
exidz <exidz@protonmail.com>
2021-06-15 23:02:26 +0800

    edit install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,6 @@ sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
 sudo apt-get install nodejs -y
 node -v
-sudo apt install git -y
-cd $HOME
-git clone https://github.com/exidz/RaisinBot.git
-cd RaisinBot
 npm install
 npm audit fix --force
 echo "Install all dependency sucessfully"
