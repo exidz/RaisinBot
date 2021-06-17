656897fd6838cb08d7304df2152817712ef99af5
exidz <exidz@protonmail.com>
2021-06-17 21:38:42 +0800

    fix big and add track slp fetaure

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
-const { Client, MessageAttachment } = require('discord.js');
+const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
 const lib = require('./qrGen')
 const data = require('./data.js')
 const client = new Client();
 
+const scholars = data.scholar();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -14,13 +15,13 @@ client.on('message', async (msg) => {
     if (msg.channel.id == data.channelID || msg.channel.type == "dm") {
         // and if message content $qr
         if(msg.content === '$qr') {
-            const scholars = data.scholar();
             scholars.map(async (scholar) => {
                 if (scholar.id === msg.author.id) {
                     const accountAddress = scholar.ethAddress;
                     const privateKey = scholar.privateKey;
                     const fileNameID = msg.author.id;
-                    const accessToken = await lib.submitSignature(accountAddress, privateKey);
+                    const randMessage = await getRawMessage();
+                    const accessToken = await lib.submitSignature(accountAddress, privateKey, randMessage);
                     await lib.generateQR(accessToken,fileNameID);
                     const attachment = new MessageAttachment('./qrcode-images/qr-'+ msg.author.id + '.png')
                     await msg.author.send("Here is your qr login code " + scholar.name)
@@ -33,6 +34,27 @@ client.on('message', async (msg) => {
         }
     
     }
+
+    if(msg.content === '!status') {
+        scholars.map(async (scholar) => {
+            if(scholar.id === msg.author.id) {
+                const accountAddress = scholar.ethAddress;
+                const status = await data.getStatus(accountAddress);
+                const embed = new MessageEmbed()
+                    .setTitle("Here is your current game status")
+                    .setAuthor(msg.author.username)
+                    .addField('Currents SLP count: ', status['total'], true)
+                    .addField('Claimable SLP: ', status['claimable_total'], true);
+
+                msg.reply(embed)
+                
+
+            } 
+        })
+
+    }
+
+
 });
 
 client.login(data.token);
