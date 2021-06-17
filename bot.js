42edae414627ac6497ed3474180038d93e1e6fd3
exidz <exidz@protonmail.com>
2021-06-17 10:16:43 +0800

    update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,8 @@
-require('dotenv').config()
 const { Client, MessageAttachment } = require('discord.js');
 const lib = require('./qrGen')
+const data = require('./data.js')
 const client = new Client();
 
-// your discord key 
-const token = process.env.DISCORD_BOT_KEY;
-// the channel id of your server where the scholar to request qr
-const channelID = process.env.CHANNEL_ID;
-//parse the process.env.SCHOLAR to make it a array of object
-const scholarDiscordID = JSON.parse(process.env.SCHOLAR)
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -17,10 +11,11 @@ client.on('ready', () => {
 client.on('message', async (msg) => {
     // A if-else statement 
     // if the scholar type the $qr command in the corresponding channel or in DM 
-    if (msg.channel.id == channelID || msg.channel.type == "dm") {
+    if (msg.channel.id == data.channelID || msg.channel.type == "dm") {
         // and if message content $qr
         if(msg.content === '$qr') {
-            scholarDiscordID.map(async (scholar) => {
+            const scholars = data.scholar();
+            scholars.map(async (scholar) => {
                 if (scholar.id === msg.author.id) {
                     const accountAddress = scholar.ethAddress;
                     const privateKey = scholar.privateKey;
@@ -40,4 +35,4 @@ client.on('message', async (msg) => {
     }
 });
 
-client.login(token);
+client.login(data.token);
