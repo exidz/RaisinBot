6787a39bd3fe19737c244005b036ce001398155a
cnuval-itrs <cnuval@itrsgroup.com>
2021-10-30 17:56:26 +0800

    support multiple currencies

--- features/getPriceSLP.js
+++ features/getPriceSLP.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch');
 
-const slpPrice = async () => {
+const slpPrice = async function getSlp(currency) {
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/coins/smooth-love-potion?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false');
         if(!response.ok) {
             throw Error('Axie Infinity API have a problem')
         }
         const price = await response.json();
-        return price["market_data"]["current_price"]["usd"];
+        return price["market_data"]["current_price"][currency];
     } catch (error) {
         console.log(error);
         return false;       
